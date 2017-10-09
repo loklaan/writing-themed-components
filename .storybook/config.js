e3805fc67eb557538ca45b175e9b63098086d6c1
Lochlan Bunn <bunn@lochlan.io>
2017-10-09 22:30:36 +1000

    Update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
+import { setOptions } from '@storybook/addon-options'
+
+setOptions({
+  name: 'Themed Components',
+  url: '#',
+  goFullScreen: false,
+  showRightPanel: false,
+  showLeftPanel: true,
+  showDownPanel: false
+})
 
 function loadStories() {
-  require('../src/stories.js');
+  require('../src/stories.js')
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
